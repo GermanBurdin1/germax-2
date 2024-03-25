86069fa15280e429adb92bc3dd6c9a6fcee7102b
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-03-25 09:52:04 +0100

    предзаполнил страницы frontend

---
+++ germax-frontend/src/templates/components/content-page-dashboard/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/api/getMenu')
+        .then(response => response.json())
+        .then(data => {
+            const menuContainer = document.getElementById('dynamicMenu');
+            data.forEach(item => {
+                const menuItem = document.createElement('a');
+                menuItem.setAttribute('href', item.url);
+                menuItem.setAttribute('class', 'list-group-item list-group-item-action');
+                menuItem.textContent = item.title;
+                menuContainer.appendChild(menuItem);
+            });
+        })
+        .catch(error => console.error('Error loading the menu:', error));
+});
---
+++ germax-frontend/src/templates/components/content-page-loans-request/index.js
@@ -0,0 +1,66 @@
+let offset = 0;
+const limit = 10;
+
+function loadModel(type = "") {
+	fetch(
+		`../../controllers/load-equipment.php?type=${type}&offset=${offset}&limit=${limit}`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			const container = document.getElementById("equipment-list");
+			container.innerHTML = "";
+			data.forEach((item) => {
+				const modelItem = `<div class="card mb-3 equipment-item" data-id="${item.id_model}">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="${item.photo}" class="card-img" alt="${item.name}">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">${item.name}</h5>
+				</div>
+			  </div>
+			</div>
+		  </div>`;
+				container.insertAdjacentHTML("beforeend", modelItem);
+			});
+
+			document.querySelectorAll(".equipment-item").forEach((card) => {
+				card.addEventListener("click", function () {
+					const id = this.getAttribute("data-id");
+					redirectToDetails(id);
+				});
+			});
+		})
+		.catch((error) => {
+			console.error("Erreur lors du chargement de l'équipement:", error);
+		});
+}
+
+document.querySelectorAll("#type-filter .list-group-item").forEach((item) => {
+	item.addEventListener("click", function (event) {
+		event.preventDefault();
+		const type = this.getAttribute("data-type");
+		loadModel(type);
+
+		document
+			.querySelectorAll("#type-filter .list-group-item")
+			.forEach((i) => i.classList.remove("active"));
+		this.classList.add("active");
+		offset = 0;
+	});
+});
+
+loadModel();
+
+document.getElementById("load-more").addEventListener("click", function () {
+	const activeType = document
+		.querySelector("#type-filter .list-group-item.active")
+		.getAttribute("data-type");
+	offset += limit;
+	loadModel(activeType);
+});
+
+function redirectToDetails(id) {
+	window.location.href = `./item.html.php?id=${id}`;
+}
