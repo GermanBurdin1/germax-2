0caeed87efa23c7332b5d775d104f34b486c8a96
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-03-27 13:42:36 +0100

    résolution de conflits

--- germax-frontend/src/templates/components/content-page-admin-reports/index.js
+++ germax-frontend/src/templates/components/content-page-admin-reports/index.js
@@ -1,1 +1,12 @@
 import './index.css';
+import Modal from "bootstrap/js/dist/modal";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const generateReportButton = document.querySelector('button[data-toggle="modal"]');
+    const generateReportModal = new Modal(document.getElementById('generateReportModal'));
+
+    generateReportButton.addEventListener('click', () => {
+        generateReportModal.show();
+    });
+
+});
--- germax-frontend/src/templates/components/content-page-admin-reservation-management/index.js
+++ germax-frontend/src/templates/components/content-page-admin-reservation-management/index.js
@@ -1,1 +1,27 @@
 import './index.css';
+import Modal from "bootstrap/js/dist/modal";
+import Collapse from "bootstrap/js/dist/collapse";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const generateReportButton = document.querySelector('button[data-toggle="modal"]');
+    const generateReportModal = new Modal(document.getElementById('generateReportModal'));
+
+    generateReportButton.addEventListener('click', () => {
+        generateReportModal.show();
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.querySelector('button[data-bs-toggle="collapse"]');
+    let collapseElement = document.getElementById('searchConflictsSection');
+    let collapse = new Collapse(collapseElement, {
+        toggle: false // Это говорит о том, что Collapse не должен автоматически переключаться при инициализации
+    });
+
+    searchButton.addEventListener('click', () => {
+        collapse.toggle(); // Это будет переключать видимость вашего collapse элемента
+    });
+
+    // Продолжение вашего кода с Modal...
+});
