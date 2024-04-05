a45b047ecad15553f747e1fdbf938d36394d4ea2
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-04-05 10:02:32 +0200

    ajouté "réservations actives et archivées" dans la rubrique de la résolution du confilt __ page-admin-reservation-management

--- germax-frontend/src/templates/components/content-page-admin-reservation-management/index.js
+++ germax-frontend/src/templates/components/content-page-admin-reservation-management/index.js
@@ -3,7 +3,7 @@ import Modal from "bootstrap/js/dist/modal";
 import Collapse from "bootstrap/js/dist/collapse";
 import Dropdown from "bootstrap/js/dist/dropdown";
 import Tab from "bootstrap/js/dist/tab";
-import { sortTable, sortOrder } from "../../../utils/sort";
+import { sortTable } from "../../../utils/sort";
 
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("Начало выполнения кода внутри DOMContentLoaded");
