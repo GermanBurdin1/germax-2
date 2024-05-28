71dabc61e253994fd17d08c56d0e205ca39e3b3f
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-05-28 13:23:19 +0200

    index page fixed

--- germax-frontend/src/templates/components/content-page-index/index.js
+++ germax-frontend/src/templates/components/content-page-index/index.js
@@ -1,16 +1,61 @@
-import './index.css';
-import { getUsers } from '../../../assets/js/get-users';
-// Пример импортов js функций
+import "./index.css";
+import Carousel from "bootstrap/js/dist/carousel";
+import Collapse from "bootstrap/js/dist/collapse";
+import Modal from "bootstrap/js/dist/modal";
 
-const scrollToFaqNode = document.querySelector('#scrollToFaq');
-const faqSectionNode = document.querySelector('#faq-section');
+const carouselElement = document.getElementById("equipmentCarousel");
+const carousel = new Carousel(carouselElement, {
+	interval: 5000,
+	wrap: true,
+});
 
-// Пример импортов js функций
-const users = getUsers();
-console.log(users, 'users');
+const links = document.querySelectorAll('a[href^="#"]');
 
-if (scrollToFaqNode !== null && faqSectionNode !== null) {
-	faqSectionNode.scrollIntoView({
-		behavior: 'smooth'
+for (const link of links) {
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+		const targetId = this.getAttribute("href").substring(1);
+		const targetElement = document.getElementById(targetId);
+
+		if (targetElement) {
+			window.scrollTo({
+				top: targetElement.offsetTop - 70,
+				behavior: "smooth",
+			});
+		}
 	});
+}
+
+const text = "Gères ta loc'";
+const typewriterText = document.getElementById("typewriter-text");
+let index = 0;
+
+function type() {
+	if (index < text.length) {
+		typewriterText.textContent += text.charAt(index);
+		index++;
+		setTimeout(type, 150);
+	} else {
+		typewriterText.classList.remove("typewriter");
+	}
+}
+
+type();
+
+const accordionElements = document.querySelectorAll(".accordion");
+accordionElements.forEach((accordion) => {
+	new Collapse(accordion, {
+		toggle: false,
+	});
+});
+
+// Initialize Modals
+const aboutModalElement = document.getElementById("aboutModal");
+if (aboutModalElement) {
+	const aboutModal = new Modal(aboutModalElement);
+}
+
+const contactModalElement = document.getElementById("contactModal");
+if (contactModalElement) {
+	const contactModal = new Modal(contactModalElement);
 }
