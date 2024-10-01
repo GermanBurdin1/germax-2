e960b0e532edf30bf7e32c8c69a700bcd90c2a05
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-01 11:05:17 +0200

    file removed from index

--- germax-api/src/equipment/description-item.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './models/EquipmentModel.php';
-
-$equipmentModel = new EquipmentModel();
-$id_model = $_GET['id'] ?? 0; // ID модели должен поступать из запроса
-$description = $equipmentModel->loadDescription($id_model);
-
-echo json_encode($description);
