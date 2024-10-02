216a0a8cefe09bd71cbc8ae4f1874f509fc27905
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-02 11:00:09 +0200

    renamed php to php-layouts

--- germax-frontend/webpack.config.js
+++ germax-frontend/webpack.config.js
@@ -187,7 +187,7 @@ const createConfig = (configDefault) => {
 		plugins: plugins,
 		output: {
 			filename: "[name].[contenthash].js",
-			path: path.resolve(__dirname, "php/dist"),
+			path: path.resolve(__dirname, "php-layouts/dist"),
 			publicPath: "/dist/",
 		},
 	};
