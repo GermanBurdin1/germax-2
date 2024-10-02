37013ccb7399b8f849a810def52b0a2efdf49a95
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-02 16:28:40 +0200

    french comments

--- germaloc-frontend/php-layouts/php-utils/serve-html-file.php
+++ germaloc-frontend/php-layouts/php-utils/serve-html-file.php
@@ -2,17 +2,12 @@
 
 function serveHtmlFile($filePath) {
 	$fullPath = $_SERVER['DOCUMENT_ROOT'] . '/' . $filePath;
-    // Проверяем, существует ли файл
     if (file_exists($fullPath)) {
-        // Устанавливаем правильный MIME тип для HTML
 		if (!headers_sent()) header('Content-Type: text/html');
-
-        // Выводим содержимое файла
         readfile($fullPath);
     } else {
-        // Если файл не найден, отправляем HTTP статус 404 Not Found
         http_response_code(404);
-        echo "Ошибка: файл не найден. Адрес $fullPath<br>";
+        echo "Erreur: fichier non trouvé. Chemin $fullPath<br>";
     }
 }
 
