16a99d27884ff525926a57ba2881990e141ca746
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-03 21:40:29 +0200

    fixed comments

--- germaloc-api/src/utils/is-json-content-type.php
+++ germaloc-api/src/utils/is-json-content-type.php
@@ -2,10 +2,8 @@
 
 function isJsonContentType()
 {
-	// Получаем все заголовки ответа
 	$headers = apache_request_headers();
 
-	// Проверяем наличие заголовка 'Content-Type' и его значение
 	return (
 		isset($headers['Content-Type']) &&
 		$headers['Content-Type'] === 'application/json'
--- germaloc-api/src/utils/render-success.php
+++ germaloc-api/src/utils/render-success.php
@@ -5,22 +5,18 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utils/is-json-content-type.php';
 
 function renderSuccessAndExit($messagesText, $code = 200, $data = [])
 {
-	// Формируем массив с данными об ошибке
 	$successData = [
 		'success' => true,
 		'messages' => [...$messagesText],
 		'data' => $data
 	];
 
-	// Проверяем, установлен ли уже заголовок Content-Type: application/json
 	if (isJsonContentType()) header('Content-Type: application/json');
 
 	http_response_code($code);
 
-	// Выводим JSON с данными об ошибке
 	echo json_encode($successData);
 
-	// Прекращаем выполнение скрипта
 	exit;
 }
 
